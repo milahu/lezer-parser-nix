07d5befc15a92b23e2969b89e6a18ff0bd8b83d5
Milan Hauth <milahu@gmail.com>
2021-11-14 13:42:14 +0100

    fix stringblock escaped interpolation

--- src/tokens.js
+++ src/tokens.js
@@ -12,7 +12,7 @@ const space = [
   8201, 8202, 8232, 8233, 8239, 8287, 12288
 ]
 
-const devMode = true; // development mode
+const devMode = false; // development mode
 
 const
   braceR = 125, braceL = 123, semicolon = 59, slash = 47, star = 42,
@@ -26,68 +26,73 @@ export const stringBlock = new ExternalTokenizer(input => {
     let {next} = input
     if (devMode) console.log([
       `\ni = ${i}`,
-      `next = ${next} = ${JSON.stringify(String.fromCharCode(next))}`,
-      `peek(1) = ${input.peek(1)} = ${JSON.stringify(String.fromCharCode(input.peek(1)))}`,
-      `peek(2) = ${input.peek(2)} = ${JSON.stringify(String.fromCharCode(input.peek(2)))}`,
+      //`peek(-2) = ${input.peek(-2)} = ${JSON.stringify(String.fromCharCode(input.peek(-2)))}`,
+      //`peek(-1) = ${input.peek(-1)} = ${JSON.stringify(String.fromCharCode(input.peek(-1)))}`,
+      `next   = ${next} = ${JSON.stringify(String.fromCharCode(next))}`,
+      `peek 1 = ${input.peek(1)} = ${JSON.stringify(String.fromCharCode(input.peek(1)))}`,
+      `peek 2 = ${input.peek(2)} = ${JSON.stringify(String.fromCharCode(input.peek(2)))}`,
       `afterQuote = ${afterQuote}`,
       `afterDollar = ${afterDollar}`
-    ].join(' + '));
+    ].map(s => `  ${s}\n`).join(''));
     if (next < 0) { // next == -1: end of file
       if (i) {
         input.acceptToken(StringBlockContent)
-        if (devMode) console.log(`  37 acceptToken(StringBlockContent)`);
+        if (devMode) console.log(`  40 acceptToken(StringBlockContent)`);
       }
-      if (devMode) console.log(`  39 break`);
+      if (devMode) console.log(`  42 break`);
       break
     } else if (next == singlequote) {
       if (input._todoStringBlockEnd) {
         // end of string
         input.advance(2)
+        if (devMode) console.log(`  63 acceptToken(stringBlockEnd), clear _todoStringBlockEnd`);
         input.acceptToken(stringBlockEnd)
-        if (devMode) console.log(`  46 acceptToken(stringBlockEnd)`);
         input._todoStringBlockEnd = false
         break
       }
-     if (input.peek() == singlequote) {
-        if (i) {
-          // end of content
-          input.acceptToken(StringBlockContent)
-          if (devMode) console.log(`  54 acceptToken(StringBlockContent) -> break`);
-          input._todoStringBlockEnd = true
+      if (input.peek() == singlequote) {
+        if (input.peek(2) == dollar) {
+          // escaped interpolation
+          if (devMode) console.log(`  56 escaped interpolation -> advance 2`);
+          input.advance(2)
         }
         else {
-          // end of string
-          input.advance(2)
-          input.acceptToken(stringBlockEnd)
-          if (devMode) console.log(`  60 acceptToken(stringBlockEnd) -> break`);
+          if (i) {
+            // end of content
+            input.acceptToken(StringBlockContent)
+            if (devMode) console.log(`  63 acceptToken(StringBlockContent) -> set _todoStringBlockEnd, break`);
+            input._todoStringBlockEnd = true
+          }
+          else {
+            // end of string
+            input.advance(2)
+            input.acceptToken(stringBlockEnd)
+            if (devMode) console.log(`  70 acceptToken(stringBlockEnd) -> break`);
+          }
+          break
         }
-        break
       }
     } else if (next == braceL && afterDollar) {
       if (i == 1) {
-        if (devMode) console.log(`  67 acceptToken(stringBlockInterpolationStart)`);
+        if (devMode) console.log(`  77 acceptToken(stringBlockInterpolationStart)`);
         input.acceptToken(stringBlockInterpolationStart, 1)
       }
       else {
-        if (devMode) console.log(`  71 acceptToken(StringBlockContent, -1)`);
+        if (devMode) console.log(`  81 acceptToken(StringBlockContent, -1)`);
         input.acceptToken(StringBlockContent, -1)
       }
-      if (devMode) console.log(`  74 break`);
+      if (devMode) console.log(`  84 break`);
       break
     } else if (next == newline && i > 0) {
       // Break up stringBlock strings on lines, to avoid huge tokens
       input.advance() // add newline to current token
-      if (devMode) console.log(`  79 acceptToken(StringBlockContent) from newline`);
+      if (devMode) console.log(`  89 acceptToken(StringBlockContent) from newline`);
       input.acceptToken(StringBlockContent)
-      if (devMode) console.log(`  81 break`);
+      if (devMode) console.log(`  91 break`);
       break
-    // FIXME escaped interpolation is ''${x} not \${x}
-    } else if (next == backslash) {
-      if (devMode) console.log(`  84 backslash advance`);
-      input.advance()
     }
     afterDollar = next == dollar
-    if (devMode) console.log(`  88 advance -> continue loop`);
+    if (devMode) console.log(`  95 advance -> continue loop`);
     input.advance()
   }
 })
