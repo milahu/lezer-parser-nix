7e8ac693e35611058de03270996a9646a6db9ccd
Milan Hauth <milahu@gmail.com>
2021-11-14 09:00:14 +0100

    fix stringblock

--- src/tokens.js
+++ src/tokens.js
@@ -11,7 +11,7 @@ const space = [
   8201, 8202, 8232, 8233, 8239, 8287, 12288
 ]
 
-const devMode = true; // development mode
+const devMode = false; // development mode
 
 const
   braceR = 125, braceL = 123, semicolon = 59, slash = 47, star = 42,
@@ -23,59 +23,69 @@ const
 export const stringBlock = new ExternalTokenizer(input => {
   for (let afterDollar = false, afterQuote = false, i = 0;; i++) {
     let {next} = input
-    if (devMode) console.log(`i = ${i} + next = ${next} = ${JSON.stringify(String.fromCharCode(next))} + afterQuote = ${afterQuote} + afterDollar = ${afterDollar}`);
+    if (devMode) console.log([
+      `\ni = ${i}`,
+      `next = ${next} = ${JSON.stringify(String.fromCharCode(next))}`,
+      `peek(1) = ${input.peek(1)} = ${JSON.stringify(String.fromCharCode(input.peek(1)))}`,
+      `peek(2) = ${input.peek(2)} = ${JSON.stringify(String.fromCharCode(input.peek(2)))}`,
+      `afterQuote = ${afterQuote}`,
+      `afterDollar = ${afterDollar}`
+    ].join(' + '));
     if (next < 0) { // next == -1: end of file
       if (i) {
         input.acceptToken(StringBlockContent)
-        if (devMode) console.log(`  acceptToken(StringBlockContent)`);
+        if (devMode) console.log(`  37 acceptToken(StringBlockContent)`);
       }
-      if (devMode) console.log(`  break 32`);
+      if (devMode) console.log(`  39 break`);
       break
     } else if (next == singlequote) {
-      if (afterQuote) {
-        if (devMode) console.log(`  found singlequote 2`);
-        input.advance()
-        if (i == 1) {
-          // empty string
-          if (devMode) console.log(`  acceptToken(stringBlockEnd) with empty string`);
-          input.acceptToken(stringBlockEnd)
-        }
-        else {
-          if (devMode) console.log(`  acceptToken(StringBlockContent, -2)`);
-          input.acceptToken(StringBlockContent, -2)
-          if (devMode) console.log(`  acceptToken(stringBlockEnd)`);
-          input.acceptToken(stringBlockEnd)
-        }
-        afterQuote = false
-        if (devMode) console.log(`  break 50`);
+      if (input._todoStringBlockEnd) {
+        // end of string
+        input.advance(2)
+        input.acceptToken(stringBlockEnd)
+        if (devMode) console.log(`  46 acceptToken(stringBlockEnd)`);
+        input._todoStringBlockEnd = false
         break
       }
-      else {
-        if (devMode) console.log(`  found singlequote 1`);
-        afterQuote = true
+     if (input.peek() == singlequote) {
+        if (i) {
+          // end of content
+          input.acceptToken(StringBlockContent)
+          if (devMode) console.log(`  54 acceptToken(StringBlockContent) -> break`);
+          input._todoStringBlockEnd = true
+        }
+        else {
+          // end of string
+          input.advance(2)
+          input.acceptToken(stringBlockEnd)
+          if (devMode) console.log(`  60 acceptToken(stringBlockEnd) -> break`);
+        }
+        break
       }
     } else if (next == braceL && afterDollar) {
       if (i == 1) {
-        if (devMode) console.log(`  acceptToken(stringBlockInterpolationStart)`);
+        if (devMode) console.log(`  67 acceptToken(stringBlockInterpolationStart)`);
         input.acceptToken(stringBlockInterpolationStart, 1)
       }
       else {
-        if (devMode) console.log(`  acceptToken(StringBlockContent, -1)`);
+        if (devMode) console.log(`  71 acceptToken(StringBlockContent, -1)`);
         input.acceptToken(StringBlockContent, -1)
       }
-      if (devMode) console.log(`  break 66`);
+      if (devMode) console.log(`  74 break`);
       break
     } else if (next == newline && i > 0) {
       // Break up stringBlock strings on lines, to avoid huge tokens
       input.advance() // add newline to current token
-      if (devMode) console.log(`  acceptToken(StringBlockContent) from newline`);
+      if (devMode) console.log(`  79 acceptToken(StringBlockContent) from newline`);
       input.acceptToken(StringBlockContent)
-      if (devMode) console.log(`  break 73`);
+      if (devMode) console.log(`  81 break`);
       break
     } else if (next == backslash) {
+      if (devMode) console.log(`  84 backslash advance`);
       input.advance()
     }
     afterDollar = next == dollar
+    if (devMode) console.log(`  88 advance -> continue loop`);
     input.advance()
   }
 })
