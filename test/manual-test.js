436b1e84669a6ddd02809b950fb703c064ee2df9
Milan Hauth <milahu@gmail.com>
2022-09-11 19:42:20 +0200

    update test/manual-test.js

--- test/manual-test.js
+++ test/manual-test.js
@@ -1,4 +1,5 @@
-import {parser as parserImported} from "../dist/index.es.js"
+import {parser as parserImported} from "../dist/index.js"
+import {stringifyTree} from "./stringify-tree.js"
 
 if (process.argv.length < 3) {
   console.log(`usage: node ${process.argv[1].split('/').pop()} "input text"`);
@@ -30,88 +31,9 @@ catch (e) {
 
 //console.dir(actual, { depth: 5 });
 
-const indentStep = "  ";
 
-// Tree https://github.com/lezer-parser/common/blob/main/src/tree.ts#L314
-actual.toString = function toString(depth = 0) {
-  //let mounted = this.prop(NodeProp.mounted)
-  //if (mounted && !mounted.overlay) return mounted.tree.toString()
-  let children = ""
-  for (let ch of this.children) {
-    let str = ch.toString(depth + 1)
-    if (str) {
-      //if (children) children += ","
-      children += str
-    }
-  }
-  return !this.type.name ? children :
-    (/\W/.test(this.type.name) && !this.type.isError ? JSON.stringify(this.type.name) : this.type.name) +
-    //(children.length ? "(" + children + ")" : "")
-    (children.length ? "\n" + children : "")
-}
-
-if (!actual.children[0].set) {
-  // Tree
-  // TODO print type + source tree
-  //console.dir(actual, { depth: 5 });
-
-  actual.children[0].toString = function toString(depth = -1) {
-    //let mounted = this.prop(NodeProp.mounted)
-    //if (mounted && !mounted.overlay) return mounted.tree.toString()
-    let children = ""
-    for (let ch of this.children) {
-      let str = ch.toString(depth + 1)
-      if (str) {
-        //if (children) children += ","
-        children += str
-      }
-    }
-    let source = text
-    let indent = indentStep.repeat(depth)
-    return indent + (!this.type.name ? children :
-      (/\W/.test(this.type.name) && !this.type.isError ? JSON.stringify(this.type.name) : this.type.name) +
-      //(children.length ? "(" + children + ")" : "")
-      ` ${source}` +
-      (children.length ? "\n" + children : ""))
-  }
-
-  console.log(actual.toString(0));
-}
-else
-if (actual.children[0].set) {
-  // TreeBuffer https://github.com/lezer-parser/common/blob/main/src/tree.ts#L530
-  // monkeypatch: print type + source tree
-  actual.children[0].toString = function toString(depth = 0) {
-    let result = []
-    for (let index = 0; index < this.buffer.length;) {
-      result.push(this.childString(index, depth + 1))
-      index = this.buffer[index + 3]
-    }
-    //return result.join(",")
-    return result.join('')
-  }
-  actual.children[0].childString = function childString(index, depth = 0) {
-    let id = this.buffer[index], endIndex = this.buffer[index + 3]
-    let type = this.set.types[id], result = type.name // TODO add source to result
-    if (/\W/.test(result) && !type.isError) result = JSON.stringify(result)
-    let source = text.slice(
-      this.buffer[index + 1],
-      this.buffer[index + 2],
-    )
-    if (/[\r\n]/.test(source)) source = JSON.stringify(source)
-    result += ` ${source}`
-    result = indentStep.repeat(depth) + result
-    index += 4
-    if (endIndex == index) return result
-    let children = []
-    while (index < endIndex) {
-      children.push(this.childString(index, depth + 1))
-      index = this.buffer[index + 3]
-    }
-    //return result + "(" + children.join(",") + ")"
-    const indent = indentStep.repeat(depth);
-    return result + '\n' + children.map(str => str + '\n').join('')
-  }
-
-  console.log(actual.toString(-1));
-}
+console.log(stringifyTree(actual));
+console.log();
+console.log(stringifyTree(actual, {pretty: true, text }));
+console.log();
+console.log(stringifyTree(actual, {human: true, text }));
