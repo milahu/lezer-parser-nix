9e1864448f64df362895033cf36012fed4bd29d1
Milan Hauth <milahu@gmail.com>
2022-09-12 19:14:07 +0200

    cleanup

--- test/update-expressions.js
+++ test/update-expressions.js
@@ -10,12 +10,12 @@ import {stringifyTree} from "./stringify-tree.js"
 //import {fileTests} from "@lezer/generator/dist/test"
 function toLineContext(file, index) {
   const endEol = file.indexOf('\n', index + 80);
-
   const endIndex = endEol === -1 ? file.length : endEol;
-
   return file.substring(index, endIndex).split(/\n/).map(str => '  | ' + str).join('\n');
 }
+
 const defaultIgnore = false
+
 function fileTests(file, fileName, mayIgnore = defaultIgnore) {
   let caseExpr = /#[ \t]*(.*)(?:\r\n|\r|\n)([^]*?)==+>([^]*?)(?:$|(?:\r\n|\r|\n)+(?=#))/gy
   let tests = []
@@ -23,8 +23,6 @@ function fileTests(file, fileName, mayIgnore = defaultIgnore) {
   for (;;) {
     let m = caseExpr.exec(file)
     if (!m) throw new Error(`Unexpected file format in ${fileName} around\n\n${toLineContext(file, lastIndex)}`)
-
-    //let [, name, configStr] = /(.*?)(\{.*?\})?$/.exec(m[1])! // typescript
     let execResult = /(.*?)(\{.*?\})?$/.exec(m[1])
     if (execResult === null) throw Error('execResult is null')
     let [, name, configStr] = execResult
@@ -33,21 +31,7 @@ function fileTests(file, fileName, mayIgnore = defaultIgnore) {
     let config = configStr ? JSON.parse(configStr) : null
     let strict = !/⚠|\.\.\./.test(expected)
 
-    tests.push({
-      name,
-      text,
-      expected,
-      configStr,
-      config,
-      strict,
-      /*
-      run(parser) {
-        if (parser.configure && (strict || config))
-          parser = parser.configure({strict, ...config})
-        testTree(parser.parse(text), expected, mayIgnore)
-      },
-      */
-    })
+    tests.push({ name, text, expected, configStr, config, strict })
     lastIndex = m.index + m[0].length
     if (lastIndex == file.length) break
   }
@@ -72,50 +56,13 @@ for (let file of fs.readdirSync(caseDir)) {
     const tree = parser.parse(testData.text);
     const stringifyOptions = writePrettyTree && { pretty: true, text };
     const newExpected = stringifyTree(tree, stringifyOptions);
-    // debug
-    //if (name == 'some test name') {
-    //  console.dir(testData)
-    //}
+    //if (name == 'some test name') { console.dir(testData) } // debug
     result.push(`# ${name}${(configStr || '')}\n${text}\n==>\n${newExpected}`)
-
     const oldExpectedErrors = (oldExpected.match(/⚠/g) || []).length;
     const newExpectedErrors = (newExpected.match(/⚠/g) || []).length;
-
     if (oldExpectedErrors != newExpectedErrors) {
       console.log(`# ${name}\n# error count changed: ${oldExpectedErrors} -> ${newExpectedErrors}\n# old expected:\n${oldExpected}\n# new expected:\n${newExpected}\n`)
     }
-
-    /* too complex. simple: log to console if error count has changed vs the last version
-    // WONTFIX?
-    // parse error -> make test fail by adding " ☹ FIXME"
-    // note: to test for parse errors, add ⚠ to the test name
-    // other error symbols: ❌ 🚫 ☹ ⚐ ⛔ 🐞
-    // note: strict is useless here
-    //   let strict = !/⚠|\.\.\./.test(expected)
-    const isFailing = /⚠|\.\.\./.test(newExpected) // TODO do we need to test for "..."?
-    const shouldFailMarker = '❌ FIXME this test should fail. if this test should pass, remove ⚠ from the test name\n';
-    const shouldPassMarker = '❌ FIXME this test should pass. if this test should fail, add ⚠ to the test name\n';
-    
-    // FIXME
-    const shouldFail = (newExpected.indexOf(shouldFailMarker) > -1);
-    const hasMarker = (newExpected.indexOf(shouldFailMarker) > -1) || (newExpected.indexOf(shouldPassMarker) > -1);
-
-    // NOTE oldExpectedErrors is a custom convention we use here,
-    // to mark tests with known parse errors.
-    // this is non-standard, but useful for this script.
-    const oldExpectedErrors = (name.match(/⚠/g) || []).length;
-    const actualErrors = (newExpected.match(/⚠/g) || []).length;
-    let actualWithFixme = newExpected;
-    if (oldExpectedErrors != actualErrors) {
-      const shouldWhat = (oldExpectedErrors > 0) ? 'fail' : 'pass';
-      const helpMessage = (
-        '\n❌ if this test should pass, remove all ⚠ from the test name.' +
-        '\n❌ if this test should fail, add ⚠ to the test name, one ⚠ for each error.'
-      );
-      actualWithFixme = `❌ FIXME this test should ${shouldWhat}. expected ${oldExpectedErrors} errors, actual ${actualErrors} errors.${helpMessage}\n` + actualWithFixme;
-    }
-    result.push(`# ${name}${(configStr || '')}\n${text}\n==>\n${actualWithFixme}`)
-    */
   }
   const newFileContent = result.join("\n\n") + "\n";
   // TODO backup?
